f68bbbd9a020913ca1a521e9acffa57f262e706b
Daniel Kamenov <sorostmcoc@gmail.com>
2024-03-11 17:45:50 +0200

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  useSession,
+} from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hello!</h1>;
+  const session = useSession();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      <h1>Hello!</h1>
+      <SignedOut>
+        <SignInButton mode="modal" ><Button>Sign In</Button></SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton><Button>Sign Out</Button></SignOutButton>
+      </SignedIn>
+      <Button>Click me</Button>
+    </main>
+  );
 }
