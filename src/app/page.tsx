4168e3b3360da4ee4744e1527415ea1f32b5a952
Daniel Kamenov <sorostmcoc@gmail.com>
2024-03-11 23:33:09 +0200

    working on header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,6 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <SignedOut>
-        <SignInButton mode="modal" ><Button>Sign In</Button></SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton><Button>Sign Out</Button></SignOutButton>
-      </SignedIn>
     </main>
   );
 }
--- src/app/statics/Header.tsx
+++ src/app/statics/Header.tsx
@@ -1,16 +1,27 @@
-import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignOutButton, SignUpButton, SignedIn, SignedOut, useSession } from "@clerk/nextjs";
+import Link from "next/link";
 
 const Header = () => {
+
   return (
-      <nav>
-        <Link href="/">
-          FrontEnderAI
-        </Link>
-        <Link href="/about">
-          About
-        </Link>
-        {/* Add more links as needed */}
-      </nav>
+    <nav className="border-b p-5">
+      <Link href="/">FrontEnderAI</Link>
+      <Link href="/about">About</Link>
+      <SignedOut>
+        <SignInButton mode="modal">
+          Login
+        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button variant="cta">Get Started</Button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+    </nav>
   );
 };
 
