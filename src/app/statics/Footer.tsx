7bbe815a20d131c57246da9fd659d247619fad62
Daniel Kamenov <sorostmcoc@gmail.com>
2024-03-18 18:40:03 +0200

    footer updates

--- src/app/statics/Footer.tsx
+++ src/app/statics/Footer.tsx
@@ -1,12 +1,21 @@
-
 const Footer = () => {
-    return (
-      <footer>
-        <p>© FrontEnderAI, 2024 </p>
-        {/* Add more footer content as needed */}
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className=" w-11/12 border-t border-slate-400 mx-auto">
+      <div className="mt-4 w-9/12 mx-auto flex justify-evenly items-center">
+        <p>© FrontEnderAI, 2024. All rights reserved. </p>
+        <div className="text-center">
+          <p>Dashboard</p>
+          <p>Pricing</p>
+          <p>Contact Us</p>
+          <p>Feedback</p>
+        </div>
+        <div className="text-center">
+          <p>Terms of Service</p>
+          <p>Privacy Policy</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
