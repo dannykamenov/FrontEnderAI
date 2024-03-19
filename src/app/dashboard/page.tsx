273793ed10b0514144b864e6b04b73f5b88a1b79
Daniel Kamenov <sorostmcoc@gmail.com>
2024-03-19 13:14:00 +0200

    users added to db on signup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/nextjs";
 
 export default function Dashboard() {
+
+    const user = useUser();
+
     return (
         <div>
         <h1>Dashboard</h1>
         <p>Welcome to the dashboard</p>
+        <Button onClick={
+            () => {
+                console.log(user);
+            }
+        }>User Info</Button>
         </div>
     );
 }
